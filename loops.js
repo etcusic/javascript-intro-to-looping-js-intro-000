a3e2889b9266ca1c1c40374f435884a35e076cb1
Eli Cusic <tntitans31@gmail.com>
2020-06-04 22:56:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ var forLoop = function(array){
     if (i === 1){
       array.push("I am 1 strange loop.")
     } else {
-      array.push(`"I am ${i} strange loops."`)
+      array.push(`I am ${i} strange loops.`)
     }
   }
   return array
