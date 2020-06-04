1997aee6d04e1357b097593a27bd0a13041d1d40
Eli Cusic <tntitans31@gmail.com>
2020-06-04 22:35:57 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 var forLoop = function(array){
-  for (var i = 0; i < 25; i++){
+  for (var i = 0; i <= 25; i++){
     var sent = `"I am ${i} strange loop${i === 0 ? '' : 's'}."`
     array.push(sent)
   }
